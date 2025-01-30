a9aafa81ba1891e5e5506e30cd6affd09a2227df
Student <lab-102@amsg.edu>
2025-01-30 16:18:31 +0500

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".toggle").click(function () {
+        $(".side-nav").toggle(300);
+    })
+    $(".close-nav").click(function () {
+        $(".side-nav").toggle(300);
+    })
+})
